5f33e2136a9981c71ee5f0ac4693cca5d8be50e6
tolian500 <tolian500@gmail.com>
2024-11-12 19:39:13 +0100

    introdution done

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,23 @@ export default {
   data() {
     return {
       name: 'John Doe',
-      status: 'pending',
-      tasks: ['Task One', 'Task Two', 'Task Three']
+      status: 'active',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://google.com',
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
     }
   }
+
 }
 
 </script>
@@ -24,6 +37,14 @@ export default {
     <li v-for="task in tasks" :key="task">{{ task }}</li>
 
   </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a>
+
+  <br />
+
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+
 
 </template>
 
